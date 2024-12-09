cb3793a6899339227251050bfeff2f97d1b0d6ed
Juliweeeee <gone9sai@gmail.com>
2024-12-09 15:16:47 +0800

    v1_張主臨

--- app/src/main/java/com/example/s1122041/MainActivity.kt
+++ app/src/main/java/com/example/s1122041/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1122041.ui.theme.S1122041Theme
 
@@ -32,10 +38,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    /*Text(
         text = "Hello $name!",
         modifier = modifier
     )
+    Image(
+        painter = painterResource(id = R.drawable.maria2),
+        contentDescription = "張主臨",
+        alpha = 0.7f,
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.Black)
+    )*/
+
 }
 
 @Preview(showBackground = true)
